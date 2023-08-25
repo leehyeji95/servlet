0212957cb19c371404a53f00893163d564a52ccf
leehyeji <leehyeji95@gmail.com>
2023-08-25 16:50:55 +0900

    프런트 컨트롤러 패턴- model 추가v3

--- src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
+++ src/main/java/hello/servlet/web/frontcontroller/v2/FrontControllerServletV2.java
@@ -30,7 +30,7 @@ public class FrontControllerServletV2 extends HttpServlet {
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        String requestURI = request.getRequestURI(); // /front-controller/v1/members/new-form
+        String requestURI = request.getRequestURI(); // /front-controller/v2/members/new-form
         // 다형성
         ControllerV2 controller = controllerMap.get(requestURI);
         if(controller == null) {
---
+++ src/main/java/hello/servlet/web/frontcontroller/v3/FrontControllerServletV3.java
@@ -0,0 +1,67 @@
+package hello.servlet.web.frontcontroller.v3;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.MyView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberFormControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberListControllerV3;
+import hello.servlet.web.frontcontroller.v3.controller.MemberSaveControllerV3;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(name = "frontControllerServletV3", urlPatterns = "/front-controller/v3/*")  // v1/ 하위에 어떤 url 도 호출된다.
+public class FrontControllerServletV3 extends HttpServlet {
+
+    // url 맵핑정보 만들기
+    private Map<String, ControllerV3> controllerMap = new HashMap<>();
+
+    public FrontControllerServletV3() {
+        // 서블릿 생성 시, mapping 정보 미리 담아두기
+        controllerMap.put("/front-controller/v3/members/new-form", new MemberFormControllerV3());
+        controllerMap.put("/front-controller/v3/members/save", new MemberSaveControllerV3());
+        controllerMap.put("/front-controller/v3/members", new MemberListControllerV3());
+    }
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String requestURI = request.getRequestURI(); // /front-controller/v3/members/new-form
+        // 다형성
+        ControllerV3 controller = controllerMap.get(requestURI);
+        if(controller == null) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        //paramMap
+        Map<String, String> paramMap = createParamMap(request);
+
+        ModelView mv = controller.process(paramMap);    // 논리이름 new-form
+        String viewName = mv.getViewName();
+        MyView view = viewResolver(viewName);
+
+        // model 을 같이 넘겨줘야한다.
+        view.render(mv.getModel(), request, response);
+
+    }
+
+    private static MyView viewResolver(String viewName) {
+        MyView view = new MyView("/WEB-INF/views/" + viewName + ".jsp");
+        return view;
+    }
+
+    private static Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+//        paramMap.forEach((key, value) -> System.out.println(paramMap.get(key).toString()));
+        return paramMap;
+    }
+}
